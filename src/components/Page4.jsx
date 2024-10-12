d13a70c143f0cb48803f006ea5d7e2fba34a186c
saidugyala <saitejarao128@gmail.com>
2024-10-11 22:15:16 -0500

    added title

--- src/components/Page4.jsx
+++ src/components/Page4.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Page4() {
   return (
     <div className="Page-content">
-      <h2>Shafiq Bio</h2>
+      <h2>Shafeeq Bio</h2>
       <p>This is the content for the Shafiq.</p>
     </div>
   );
